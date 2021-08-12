891c8c726beb47b83d1f115dd8ae5edd99186a5b
QuintonO14 <quintonoconno14@gmail.com>
2021-08-12 14:02:38 -0600

    Bug Fix

--- pages/signIn.js
+++ pages/signIn.js
@@ -24,7 +24,7 @@ export default function SignIn({providers}) {
       <h2>Sign In</h2>
      {Object.values(providers).map(provider => (
         <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
+          <button onClick={() => signIn(provider.id, {callbackUrl: 'https://thedevsocial.vercel.app/'})}>
             Connect with {provider.name}
           </button>
         </div>
